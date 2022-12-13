3b6774d9017f01274aa6235f8f2808d11823d266
Joy Paul <joypaul3592@gmail.com>
2022-12-14 00:20:31 +0600

    addnig student

--- src/Components/Home/Testmonial/CarosuleItem/CarosuleItem.js
+++ src/Components/Home/Testmonial/CarosuleItem/CarosuleItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import reviewImg from '../../../../Assect/reviewImg.jpg'
-const CarosuleItem = () => {
+const CarosuleItem = ({ studentData }) => {
+
+
     return (
         <div className=" h-full py-10  ">
             <div class=" bg-no-repeat h-full flex justify-center items-center">
@@ -8,24 +10,27 @@ const CarosuleItem = () => {
                     <figure class="relative w-40 h-40 m-0 mx-auto rounded-full outline outline-offset-4 outline-purple-600  before:content-[''] before:absolute before:block before:pointer-events-none before:rounded-full before:h-full before:w-full before:bg-fuchsia-200 before:-z-[1] group-hover:before:scale-[2.28] motion-safe:before:transition-all  motion-safe:transform-gpu motion-safe:before:duration-500 before:origin-center group-hover:outline-white"
                     >
                         <img
-                            class="rounded-full block max-w-full bg-purple-600 object-cover z-10 relative"
-                            src={reviewImg}
+                            class="rounded-full block max-w-full bg-purple-600 h-full object-cover z-10 relative"
+                            src={studentData?.img}
                             alt="Avatar"
                         />
                     </figure>
                     <header class="motion-safe:translate-y-4 group-hover:translate-y-0 motion-safe:transition-transform motion-safe:transform-gpu motion-safe:duration-500">
                         <h3 class="font-semibold text-2xl text-center text-purple-600 mt-6   relative">
-                            আনিফা আক্তার মিম
+                            {studentData?.name}
                         </h3>
                         <p class="text-center group-hover:text-gray-700   relative">
-                            খুলনা নার্সিং কলেজ, খুলনা
+                            {studentData?.collage}
                         </p>
                     </header>
 
                     <div class="flex justify-center  mt-16 text-gray-700 relative">
                         <div class="motion-safe:translate-y-[150px]  group-hover:translate-y-[-40px] motion-safe:delay-100 motion-safe:transition">
-                            <span className=" text-justify text-ellipsis overflow-hidden ... ">আমি কলি হাওলাদার। সৃষ্টিকর্তার অসীম কৃপায় আমি ২০১৯-২০ শিক্ষাবর্ষে খুলনা নার্সিং কলেজ-এ চান্স পেয়েছি।
-                                আমার এই সা.. </span>
+                            <span className=" text-justify text-ellipsis overflow-hidden ... ">
+                                {
+                                    studentData?.review.slice(0, 115)
+                                }
+                                .. </span>
                             <button className=" ml-1 text-purple-600">আরো পড়ুন</button>
                         </div>
 
